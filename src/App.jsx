ed801982773fd2445e058e177ec20f1a448fd68f
vecvecv <valdiviezocristinae@gmail.com>
2024-03-06 01:24:19 -0300

    reto5

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import NuevoComp from './NuevoComp'
+
+function App() {
+
+  return(
+    <>
+    <h1>Hola Mundo desde React</h1>
+    <p>Te queremos Vite y React</p>
+    <NuevoComp></NuevoComp>
+    </>
+  )
+}
+
+export default App
---
+++ src/NuevoComp.jsx
@@ -0,0 +1,10 @@
+function NuevoComp() {
+    return ( 
+        <>
+        <h1>Componente Nuevo</h1>
+        
+        </>
+     );
+}
+
+export default NuevoComp;
